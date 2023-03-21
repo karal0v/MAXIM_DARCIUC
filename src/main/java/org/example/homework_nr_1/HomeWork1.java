2dbb4cb52c1203c893584c3984687ce4ba6d33f4
karal0v <darciuc2403@gmail.com>
2023-03-21 22:32:00 +0200

    My homework_nr_1 is done.

---
+++ src/main/java/org/example/homework_nr_1/HomeWork1.java
@@ -0,0 +1,37 @@
+package org.example.homework_nr_1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterInAlphabet = 'A';
+        boolean JavaIsAmazing = true;
+        double Pi = 3.14159;
+
+        System.out.println("CurrentWorldPopulation = " + CurrentWorldPopulation);
+        System.out.println("CarsProducedThisYear = " + CarsProducedThisYear);
+        System.out.println("FirstLetterInAlphabet = " + FirstLetterInAlphabet);
+        System.out.println("JavaIsAmazing = " + JavaIsAmazing);
+        System.out.println("Pi = " + Pi);
+
+        System.out.println("------------------------------------");
+
+        double Price = 3.50;
+        double Tax = 1.30;
+        int Quantity = 1;
+        double Sum = 0;
+        System.out.println("Price = "+Price);
+        System.out.println("Tax = "+Tax);
+        System.out.println("Quantity = "+Quantity);
+        System.out.println("Sum = "+Sum);
+
+        double Total = (Price + Tax * Quantity + Sum);
+        System.out.println("Total = "+Total);
+
+
+
+    }
+}
+
