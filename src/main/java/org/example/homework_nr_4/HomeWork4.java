2f628936562b6fcb15b6272297722f5f2c61a883
maxs <110688809+karal0v@users.noreply.github.com>
2023-04-06 22:08:30 +0300

    Homework nr 4 (#4)
    * Modified the number "1" to the number "2" in first-file.txt
    
    * Modified the number "2" to the number "3" in first-file.txt
    
    * My homework_nr_1 is done.
    
    * My homework_nr_2 is for half done.
    
    * Added nr.5 in file homework_nr_2.
    
    * My homework_nr_2 is done.
    
    * My homework_nr_2 is done.
    
    * My homework_nr_2 is done.
    
    * My homework_nr_2 is done.
    
    * My homework_nr_3 is done.
    
    * My homework_nr_4 is done.
    
    * My homework_nr_4 is done.

---
+++ src/main/java/org/example/homework_nr_4/HomeWork4.java
@@ -0,0 +1,43 @@
+package org.example.homework_nr_4;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 5, 6, 7};
+        for (int a = 0; a != array.length; a++)
+            System.out.println(array[a]);
+        System.out.println("------------------------------------------");
+
+        int Summa = 0;
+        for (int x = 0; x != array.length; x++)
+            Summa += array[x];
+        System.out.println("Summa: " + Summa);
+
+        float Average = Summa / array.length;
+        System.out.println("Average: " + Average);
+        System.out.println("-------------------------------------------");
+
+        int[][] array2 = new int[][]{
+                {0, 5, 7, 4, 6},
+                {6, 4, 6, 2, 1}
+        };
+        int count1 = 0;
+        int count2 = 0;
+        for (int z = 0; z != array2.length; z++)
+            for (int v = 0; v != array2[z].length; v++)
+                if (array2[z][v] % 2 == 0)
+                    count1++;
+
+                else count2++;
+        System.out.println("%2==0: " + count1);
+
+        System.out.println("%2!=0: " + count2);
+
+        System.out.println("------------------------------------------------------");
+
+        String[] namesArray = new String[]{"Vasily", "Maxim", "Mihail", "Gherman", "Pedro"};
+        for (int index = 0; index != namesArray.length; index++) {
+            System.out.println(namesArray[index]);
+        }
+        System.out.println("------------------------------------------------------");
+    }
+}
