7e87d439e6c9bf1e6ce58d2734f51dda89743341
karal0v <darciuc2403@gmail.com>
2023-04-09 22:47:50 +0300

    My homework_nr_5 is done.

---
+++ src/main/java/org/example/homework_nr_5/HomeWork5.java
@@ -0,0 +1,70 @@
+package org.example.homework_nr_5;
+
+import java.time.Month;
+
+public class HomeWork5 {
+    public static void main(String[] args) {
+        Month[] months = Month.values();
+        int i;
+        for (i = 0; i != Month.values().length; i++)
+            System.out.println(Month.values()[i]);
+
+        enum Month {
+            JANUARY,
+            FEBRUARY,
+            MARCH,
+            APRIL,
+            MAY,
+            JUNE,
+            JULY,
+            AUGUST,
+            SEPTEMBER,
+            OCTOBER,
+            NOVEMBER,
+            DECEMBER
+
+        }
+        for (Month m : Month.values()) {
+            System.out.println(m);
+
+        System.out.println("-------------------------------------------");
+
+        enum WeekDay {
+            MONDAY,
+            TUESDAY,
+            WEDNESDAY,
+            THURSDAY,
+            FRIDAY,
+            SATURDAY,
+            SUNDAY;
+
+
+            public boolean isWeekday() {
+                return this != SATURDAY && this != SUNDAY;
+            }
+
+            public boolean isHoliday() {
+                return !isWeekday();
+            }
+
+
+        }
+
+
+            System.out.println(WeekDay.SATURDAY.isWeekday()); // true
+            System.out.println(WeekDay.SATURDAY.isWeekday()); // false
+            System.out.println(WeekDay.SATURDAY.isHoliday()); // true
+
+            System.out.println("----------------------------------------------");
+
+            boolean boolVar = Boolean.parseBoolean("true");
+            byte byteVar = Byte.parseByte("120");
+            short shortVar = Short.parseShort("32000");
+            int intVar = Integer.parseInt("147895632");
+            long longVar = Long.parseLong("987654321987654321");
+            double doubleVar = Double.parseDouble("4444.5d");
+            float floatVar = Float.parseFloat("2354.56f");
+
+        }
+    }
+}
